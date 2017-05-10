53691a48c6a6779e65682ab621b233477d58701f
Magmodules <info@magmodules.eu>
2017-05-10 12:26:04 +0200

    1.5.3

--- app/code/community/Magmodules/Feedbackcompany/Model/Observer.php
+++ app/code/community/Magmodules/Feedbackcompany/Model/Observer.php
@@ -72,7 +72,7 @@ class Magmodules_Feedbackcompany_Model_Observer
         $storeIds = Mage::getModel('feedbackcompany/api')->getStoreIds('prcron');
         foreach ($storeIds as $storeId) {
             $startTime = microtime(true);
-            $results = Mage::getModel('feedbackcompany/productreviews')->runUpdate($storeId);
+            $results = Mage::getModel('feedbackcompany/productreviews')->runUpdate($storeId, 'last_week');
             $logModel->addToLog('productreviews', $storeId, $results, '', $startTime, 'productreviews');
         }
     }
